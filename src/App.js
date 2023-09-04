0da217b516fce262bce18848221cea2452aad28f
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-04 15:36:28 -0500

    working on the landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+import { useAutState, useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import { useCollectionDate } from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAP0PzSl1hbPQkWI33O5C5hNwUErO9WgY0",
+
+  authDomain: "fullstackchatapp-c64e6.firebaseapp.com",
+
+  projectId: "fullstackchatapp-c64e6",
+
+  storageBucket: "fullstackchatapp-c64e6.appspot.com",
+
+  messagingSenderId: "194250480716",
+
+  appId: "1:194250480716:web:9f8b94a5b123b9f3a4e8e9"
+
+})
+
 function App() {
+
+  const [user] = useAutState(auth)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+        <section>
+          {user ? <ChatRoom /> : <SignIn />}
+        </section>
+        
       </header>
     </div>
   );
+}
+
+function SignIn() {
+
+  const useSignInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    auth.signInWithPopup(provider)
+  }
+
+  return (
+    <button onClick={useSignInWithGoogle}>Sign in with Google</button>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom() {
+
 }
 
 export default App;
